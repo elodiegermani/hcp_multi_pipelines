a2c24d1658b7f3f46c7dd6209e11eedbd61cfaef
elodiegermani1 <elodie.germani@irisa.fr>
2022-10-11 11:20:30 +0200

    Igrida launcher

--- src/igrida_launcher.sh
+++ src/igrida_launcher.sh
@@ -4,6 +4,16 @@
 expe_name="hcp_pipelines"
 main_script=/srv/tempdd/egermani/hcp_pipelines/src/singularity_launcher.sh
 
+
+echo "Create dir for log"
+CURRENTDATE=`date +"%Y-%m-%d"`
+echo "currentDate :"
+echo $CURRENTDATE
+PATHLOG="/srv/tempdd/egermani/Logs/${CURRENTDATE}_OARID_${OAR_JOB_ID}/"
+echo "path log :"
+echo $PATHLOG
+mkdir $PATHLOG
+
 . /etc/profile.d/modules.sh
 
 set -x
