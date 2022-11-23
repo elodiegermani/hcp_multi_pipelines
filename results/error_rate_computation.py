d03d2a5757736484222336cab62bcf452457af43
elodiegermani1 <elodie.germani@irisa.fr>
2022-11-23 15:29:22 +0100

    Error rate computation

--- results/error_rate_computation.py
+++ results/error_rate_computation.py
@@ -67,11 +67,11 @@ def compute_error_rate(result_dir, n_iter, contrast_list):
         for i in range(n_iter):
             # Files for contrast and iteration
             stat_map_1 = opj(result_dir, f'final_results_group_comparison_999', 'l2_analysis', f'_contrast_{contrast}', 
-                          f'_n_{n_iter}', '_threshold0', 'spmT_0001_thr.nii')
+                          f'_n_{i}', '_threshold0', 'spmT_0001_thr.nii')
             stat_map_2 = opj(result_dir, f'final_results_group_comparison_999', 'l2_analysis', f'_contrast_{contrast}',
-                          f'_n_{n_iter}', '_threshold1', 'spmT_0002_thr.nii')
+                          f'_n_{i}', '_threshold1', 'spmT_0002_thr.nii')
             mask = opj(opj(result_dir, f'final_results_group_comparison_999', 'l2_analysis', f'_contrast_{contrast}',
-                          f'_n_{n_iter}', 'mask.nii'))
+                          f'_n_{i}', 'mask.nii'))
             # Remove NaNs
             stat_map_1_data = np.nan_to_num(nib.load(stat_map_1).get_fdata()) 
             stat_map_2_data = np.nan_to_num(nib.load(stat_map_2).get_fdata()) 
