760f434426dc2a93230ff5d9bb65a4c12f053c67
elodiegermani1 <elodie.germani@irisa.fr>
2022-11-24 16:48:27 +0100

    Registration

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
 
     if 'registration' in operation:
         registration = pipeline.get_registration(exp_dir, output_dir, working_dir, result_dir, subject_list, task_list, 
-            contrast_list, fwhm_list, nb_param)
+            contrast_list, fwhm_list, nb_param, hrf)
 
         registration.run('MultiProc', plugin_args={'n_procs': 16})
 
